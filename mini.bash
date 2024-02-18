9e69b76626ac6bff115310d27a4f2d772264fad1
Quitaxd <vkramazan2@gmail.com>
2024-02-18 15:11:06 +0300

    Update mini.bash

--- mini.bash
+++ mini.bash
@@ -6,21 +6,26 @@ USERNAME=1
 OS=1
 PROCESSOR=1
 KERNEL=1
+ALL=0
 
 [ -f ~/.config/minifetch/config ] && source ~/.config/minifetch/config
 
-if [ $USERNAME = 1 ]; then
+if [ $USERNAME == 1 ]; then
 				echo "	${USER}@$(uname -n)"
 fi
 
-if [ $OS = 1 ]; then
+if [ $OS == 1 ]; then
 				echo "Operating System: $(lsb_release -sd 2>/dev/null)"
 fi
 
-if [ $PROCESSOR = 1 ]; then
+if [ $PROCESSOR == 1 ]; then
 				echo "Processor: $(uname -p)"
 fi
 
-if [ $KERNEL = 1 ]; then
+if [ $KERNEL == 1 ]; then
 				echo "Kernel: $(uname -r)"
+fi
+
+if [ $ALL == 1]; then
+				echo "All Things: $(uname -a)"
 fi
