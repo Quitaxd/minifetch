8bac2f3f5c43730ca5f2c75f6dfc0e03b3c87555
Quitaxd <vkramazan2@gmail.com>
2024-02-18 14:52:06 +0300

    Initial Commit

---
+++ mini.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+
+# Settings
+USERNAME=1
+OS=1
+PROCESSOR=1
+KERNEL=1
+
+[ -f ~/.config/minifetch/config ] && source ~/.config/minifetch/config
+
+if [ $USERNAME = 1 ]; then
+				echo "	${USER}@$(uname -n)"
+fi
+
+if [ $OS = 1 ]; then
+				echo "Operating System: $(lsb_release -sd 2>/dev/null)"
+fi
+
+if [ $PROCESSOR = 1 ]; then
+				echo "Processor: $(uname -p)"
+fi
+
+if [ $KERNEL = 1 ]; then
+				echo "Kernel: $(uname -r)"
+fi
